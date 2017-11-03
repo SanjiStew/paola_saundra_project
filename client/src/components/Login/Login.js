5649d749781d58ff8a493cb4ddfa15de620b844e
Saundra Stewart <stewart.saundra@gmail.com>
2017-11-02 19:30:17 -0600

    created login page

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Login.css';
+class Login extends Component {
+    render () {
+        return (
+            <div>
+               <div className="container-fluid">
+                    <div className="row">
+                   
+                        <div className="left col-md-3"></div>
+                        <div className="center col-md-6"></div>
+                        <div className="right col-md-3"></div>
+                    </div> 
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
