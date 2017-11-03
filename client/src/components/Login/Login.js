778ebdcadd7dda4a542899e8727e72cd0abf0f9f
Saundra Stewart <stewart.saundra@gmail.com>
2017-11-02 20:28:35 -0600

    login page changes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import './Login.css';
-import '../../assets/auth_logo.png';
+import Logo from '../../assets/auth_logo.png';
 import Input from '../Input/Input';
 import Button from '../Buttons/Button';
 class Login extends Component {
     render () {
         return (
             <div>
-               <div className="container-fluid">
-                    <div className="row">
-                   
-                        <div className="left col-md-3"></div>
-                        <div className="center col-md-6">
-
-                        </div>
-                        <div className="right col-md-3"></div>
-                    </div> 
-                </div>
+              
             </div>
         );
     }
