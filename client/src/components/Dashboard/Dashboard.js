9441be8c4da2288c121efa7ddca7624bc4d47a0e
Paola Anderson <pbcn1984@gmail.com>
2017-11-02 19:06:35 -0600

    added login component

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -9,20 +9,14 @@ class Dashboard extends Component {
                 <Header />
                 <div className="container-fluid">
                     <div className="row">
-
-                        <div className="left col-sm">
-
-                        </div>
-                        <div className="center col-md">
-
-                        </div>
-                        <div className="right col-sm">
-
-                        </div>
+                   
+                        <div className="left col-md-3"></div>
+                        <div className="center col-md-6"></div>
+                        <div className="right col-md-3"></div>
+                    </div>
                     </div>
                 </div>
-            </div>
-
+            
         );
     }
 }
