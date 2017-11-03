22b7d615ecd4a2ed1a6a2e7e148db6310106d363
Paola Anderson <pbcn1984@gmail.com>
2017-11-02 18:33:35 -0600

    dashcboard css

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,11 +3,24 @@ import './Dashboard.css';
 import Header from './../Header/Header';
 
 class Dashboard extends Component {
-    render(){
-        return(
-        <div>
-            <Dashboard/>
-        </div>
+    render() {
+        return (
+            <div>
+                <Header />
+                <div class="container">
+                    <div class="row">
+                        <div class="left col-sm">
+                            One of three columns
+                        </div>
+                        <div class="center col-md">
+                            One of three columns
+                         </div>
+                        <div class="right col-sm">
+                            One of three columns
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
